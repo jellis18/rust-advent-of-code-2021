ecf7b69f9ddb3a00af3bd7b3c3aca6e1d19adbe3
Justin Ellis <justin.ellis18@gmail.com>
2021-12-01 08:54:27 -0500

    refactor and add tests

--- day01a/src/main.rs
+++ day01a/src/main.rs
@@ -1,13 +1,25 @@
+fn get_count_of_increased_depths(depths: Vec<usize>) -> usize {
+    depths
+        .iter()
+        .zip(depths.iter().skip(1))
+        .filter(|(x, y)| y > x)
+        .count()
+}
+
 fn main() {
     let depths: Vec<usize> = include_str!("../input.txt")
         .lines()
         .map(|x| x.parse::<usize>().unwrap())
         .collect();
 
-    let count_increased = depths
-        .iter()
-        .zip(depths.iter().skip(1))
-        .filter(|(x, y)| y > x)
-        .count();
-    println!("Depth increased {0} times", count_increased);
+    println!(
+        "Depth increased {0} times",
+        get_count_of_increased_depths(depths)
+    );
+}
+
+#[test]
+fn test_count_increased() {
+    let depths = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+    assert_eq!(7, get_count_of_increased_depths(depths))
 }
--- day01b/src/main.rs
+++ day01b/src/main.rs
@@ -1,17 +1,27 @@
-fn main() {
-    let depth_windows: Vec<usize> = include_str!("../input.txt")
-        .lines()
-        .map(|x| x.parse::<usize>().unwrap())
-        .collect::<Vec<usize>>()
-        .windows(3)
-        .map(|w| w.iter().sum::<usize>())
-        .collect();
+fn get_count_of_increased_depths_by_window(depths: Vec<usize>) -> usize {
+    let depth_windows: Vec<usize> = depths.windows(3).map(|w| w.iter().sum::<usize>()).collect();
 
-    let depth_window_increased = depth_windows
+    depth_windows
         .iter()
         .zip(depth_windows.iter().skip(1))
         .filter(|(x, y)| y > x)
-        .count();
+        .count()
+}
 
-    println!("Depth window increased {} times", depth_window_increased)
+fn main() {
+    let depths: Vec<usize> = include_str!("../input.txt")
+        .lines()
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect();
+
+    println!(
+        "Depth window increased {} times",
+        get_count_of_increased_depths_by_window(depths)
+    )
+}
+
+#[test]
+fn test_count_increased() {
+    let depths = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+    assert_eq!(5, get_count_of_increased_depths_by_window(depths))
 }
