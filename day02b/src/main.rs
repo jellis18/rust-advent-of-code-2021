bf1c4f97c0dc140a58802beb5d90c200ca614731
Justin Ellis <justin.ellis18@gmail.com>
2021-12-02 08:06:37 -0500

    Adding day 2

---
+++ day02b/src/main.rs
@@ -0,0 +1,45 @@
+fn get_product_of_headings(headings: Vec<(&str, i32)>) -> i32 {
+    let mut depth = 0;
+    let mut distance = 0;
+    let mut aim = 0;
+    for (direction, value) in headings {
+        if direction == "forward" {
+            distance += value;
+            depth += aim * value;
+        }
+        if direction == "down" {
+            aim += value
+        }
+        if direction == "up" {
+            aim -= value
+        }
+    }
+    depth * distance
+}
+
+fn main() {
+    let headings: Vec<(&str, i32)> = include_str!("../input.txt")
+        .lines()
+        .map(|line| line.split(' ').collect::<Vec<&str>>())
+        .map(|v| (v[0], v[1].parse::<i32>().unwrap()))
+        .collect();
+
+    println!(
+        "Product of direction and depth is {}",
+        get_product_of_headings(headings)
+    )
+}
+
+#[test]
+fn test_product_of_headings() {
+    let input = vec![
+        ("forward", 5),
+        ("down", 5),
+        ("forward", 8),
+        ("up", 3),
+        ("down", 8),
+        ("forward", 2),
+    ];
+
+    assert_eq!(900, get_product_of_headings(input))
+}
